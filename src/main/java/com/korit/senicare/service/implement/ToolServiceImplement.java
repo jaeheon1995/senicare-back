cb554865c7dd00e593d95bd1fb923597eb2e3e47
jaeheon jeong <huchehuche@naver.com>
2024-09-20 12:01:15 +0900

    240730.0920

--- src/main/java/com/korit/senicare/service/implement/ToolServiceImplement.java
+++ src/main/java/com/korit/senicare/service/implement/ToolServiceImplement.java
@@ -6,18 +6,20 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.korit.senicare.dto.request.tool.PatchToolRequestDto;
 import com.korit.senicare.dto.request.tool.PostToolRequestDto;
 import com.korit.senicare.dto.response.ResponseDto;
 import com.korit.senicare.dto.response.tool.GetToolListResponseDto;
+import com.korit.senicare.dto.response.tool.GetToolResponseDto;
 import com.korit.senicare.entity.ToolEntity;
 import com.korit.senicare.repository.ToolRepository;
-import com.korit.senicare.service.ToolSerivce;
+import com.korit.senicare.service.ToolService;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class ToolServiceImplement implements ToolSerivce {
+public class ToolServiceImplement implements ToolService {
 
     private final ToolRepository toolRepository;
 
@@ -53,6 +55,64 @@ public class ToolServiceImplement implements ToolSerivce {
         }
 
         return GetToolListResponseDto.success(toolEntities);
+
+    }
+
+    @Override
+    public ResponseEntity<? super GetToolResponseDto> getTool(Integer toolNumber) {
+        
+        ToolEntity toolEntity = null;
+
+        try {
+
+            toolEntity = toolRepository.findByToolNumber(toolNumber);
+            if (toolEntity == null) return ResponseDto.noExistTool();
+            
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+
+        return GetToolResponseDto.success(toolEntity);
+
+    }
+
+    @Override
+    public ResponseEntity<ResponseDto> patchTool(Integer toolNumber, PatchToolRequestDto dto) {
+        
+        try {
+
+            ToolEntity toolEntity = toolRepository.findByToolNumber(toolNumber);
+            if (toolEntity == null) return ResponseDto.noExistTool();
+
+            toolEntity.patch(dto);
+            toolRepository.save(toolEntity);
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+
+        return ResponseDto.success();
+
+    }
+
+    @Override
+    public ResponseEntity<ResponseDto> deleteTool(Integer toolNumber) {
+        
+        try {
+
+            ToolEntity toolEntity = toolRepository.findByToolNumber(toolNumber);
+            if (toolEntity == null) return ResponseDto.noExistTool();
+
+            toolRepository.delete(toolEntity);
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+
+        return ResponseDto.success();
 
     }
 
